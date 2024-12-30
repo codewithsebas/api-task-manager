8534366cdb0ecb922ee93c4c3d7edf47dd9e6692
Sebastian Giraldo <joabgiraldo@gmail.com>
2024-12-30 15:11:35 -0500

    feat: reconfig

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: process.env.WEB_URL || 5000,
+  origin: process.env.API_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 };
@@ -23,7 +23,7 @@ setupSwagger(app);
 
 app.use('/api/tasks', taskRoutes);
 
-const PORT = process.env.API_URL || 5000;
+const PORT = process.env.API_URL;
 const server = app.listen(PORT, () => {
   console.log(`Servidor corriendo en ${PORT}`);
 });
